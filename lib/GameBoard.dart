3abe50e19d51bf432eb2be996368c76e75de813f
Mikhail Vodeneev <vodeneevm@mail.ru>
2024-01-28 20:31:51 +0300

    create GameBoard

--- lib/GameBoard.dart
+++ lib/GameBoard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tetris/GridCell.dart';
 
 class GameBoard extends StatefulWidget
 {
@@ -10,9 +11,26 @@ class GameBoard extends StatefulWidget
 
 class _GameBoardState extends State<GameBoard>
 {
+  int rowLength = 10;
+  int colLength = 15;
+
   @override
   Widget build(BuildContext context)
   {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: GridView.builder(
+        itemCount: rowLength * colLength,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: rowLength),
+        itemBuilder: (context, index) => Center(
+          child: GridCell(
+              color: Colors.grey[900],
+              child: index,
+          ),
+        ),
+      ),
+    );
   }
 }
